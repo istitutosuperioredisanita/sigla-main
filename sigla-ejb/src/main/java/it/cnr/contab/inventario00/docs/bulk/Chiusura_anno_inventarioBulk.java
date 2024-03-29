b1d07cb7df1dc174258e9078902171c9ad4b3868
salvio_ciro <cirosalvio@hotmail.com>
2024-03-29 13:37:04 +0100

    chiusura inventario

---
+++ sigla-ejb/src/main/java/it/cnr/contab/inventario00/docs/bulk/Chiusura_anno_inventarioBulk.java
@@ -0,0 +1,141 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 27/03/2024
+ */
+package it.cnr.contab.inventario00.docs.bulk;
+
+import it.cnr.contab.config00.pdcep.bulk.Voce_epBulk;
+import it.cnr.contab.docamm00.tabrif.bulk.Categoria_gruppo_inventBulk;
+import it.cnr.contab.inventario00.tabrif.bulk.Tipo_ammortamentoBulk;
+public class Chiusura_anno_inventarioBulk extends Chiusura_anno_inventarioBase {
+	/**
+	 * [CATEGORIA_GRUPPO_INVENT ]
+	 **/
+	private Categoria_gruppo_inventBulk categoriaGruppoInvent =  new Categoria_gruppo_inventBulk();
+	/**
+	 * [VOCE_EP ]
+	 **/
+	private Voce_epBulk voceEp =  new Voce_epBulk();
+	/**
+	 * [TIPO_AMMORTAMENTO ]
+	 **/
+	private Tipo_ammortamentoBulk tipoAmmortamento =  new Tipo_ammortamentoBulk();
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CHIUSURA_ANNO_INVENTARIO
+	 **/
+	public Chiusura_anno_inventarioBulk() {
+		super();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CHIUSURA_ANNO_INVENTARIO
+	 **/
+	public Chiusura_anno_inventarioBulk(Long pgChiusura, Integer anno, String tipoChiusura, String cdCategoriaGruppo) {
+		super(pgChiusura, anno, tipoChiusura, cdCategoriaGruppo);
+		setCategoriaGruppoInvent( new Categoria_gruppo_inventBulk(cdCategoriaGruppo) );
+	}
+	public Categoria_gruppo_inventBulk getCategoriaGruppoInvent() {
+		return categoriaGruppoInvent;
+	}
+	public void setCategoriaGruppoInvent(Categoria_gruppo_inventBulk categoriaGruppoInvent)  {
+		this.categoriaGruppoInvent=categoriaGruppoInvent;
+	}
+	public Voce_epBulk getVoceEp() {
+		return voceEp;
+	}
+	public void setVoceEp(Voce_epBulk voceEp)  {
+		this.voceEp=voceEp;
+	}
+	public Tipo_ammortamentoBulk getTipoAmmortamento() {
+		return tipoAmmortamento;
+	}
+	public void setTipoAmmortamento(Tipo_ammortamentoBulk tipoAmmortamento)  {
+		this.tipoAmmortamento=tipoAmmortamento;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Codice categoria/gruppo]
+	 **/
+	public String getCdCategoriaGruppo() {
+		Categoria_gruppo_inventBulk categoriaGruppoInvent = this.getCategoriaGruppoInvent();
+		if (categoriaGruppoInvent == null)
+			return null;
+		return getCategoriaGruppoInvent().getCd_categoria_gruppo();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Codice categoria/gruppo]
+	 **/
+	public void setCdCategoriaGruppo(String cdCategoriaGruppo)  {
+		this.getCategoriaGruppoInvent().setCd_categoria_gruppo(cdCategoriaGruppo);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Esercizio voce]
+	 **/
+	public Integer getEsercizioVoce() {
+		Voce_epBulk voceEp = this.getVoceEp();
+		if (voceEp == null)
+			return null;
+		return getVoceEp().getEsercizio();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Esercizio voce]
+	 **/
+	public void setEsercizioVoce(Integer esercizioVoce)  {
+		this.getVoceEp().setEsercizio(esercizioVoce);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Codice Identificativo completo del conto del piano.]
+	 **/
+	public String getCdVoceEp() {
+		Voce_epBulk voceEp = this.getVoceEp();
+		if (voceEp == null)
+			return null;
+		return getVoceEp().getCd_voce_ep();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Codice Identificativo completo del conto del piano.]
+	 **/
+	public void setCdVoceEp(String cdVoceEp)  {
+		this.getVoceEp().setCd_voce_ep(cdVoceEp);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Codice Tipo Ammortamento]
+	 **/
+	public String getCdTipoAmmortamento() {
+		Tipo_ammortamentoBulk tipoAmmortamento = this.getTipoAmmortamento();
+		if (tipoAmmortamento == null)
+			return null;
+		return getTipoAmmortamento().getCd_tipo_ammortamento();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Codice Tipo Ammortamento]
+	 **/
+	public void setCdTipoAmmortamento(String cdTipoAmmortamento)  {
+		this.getTipoAmmortamento().setCd_tipo_ammortamento(cdTipoAmmortamento);
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Tipo Ammortamento]
+	 **/
+	public String getTiAmmortamento() {
+		Tipo_ammortamentoBulk tipoAmmortamento = this.getTipoAmmortamento();
+		if (tipoAmmortamento == null)
+			return null;
+		return getTipoAmmortamento().getTi_ammortamento();
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Tipo Ammortamento]
+	 **/
+	public void setTiAmmortamento(String tiAmmortamento)  {
+		this.getTipoAmmortamento().setTi_ammortamento(tiAmmortamento);
+	}
+}
---
+++ sigla-ejb/src/main/java/it/cnr/contab/inventario00/docs/bulk/Chiusura_anno_inventarioKey.java
@@ -0,0 +1,101 @@
+/*
+ * Created by BulkGenerator 2.0 [07/12/2009]
+ * Date 27/03/2024
+ */
+package it.cnr.contab.inventario00.docs.bulk;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.persistency.KeyedPersistent;
+public class Chiusura_anno_inventarioKey extends OggettoBulk implements KeyedPersistent {
+	private Long pgChiusura;
+	private Integer anno;
+	private String tipoChiusura;
+	private String cdCategoriaGruppo;
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Table name: CHIUSURA_ANNO_INVENTARIO
+	 **/
+	public Chiusura_anno_inventarioKey() {
+		super();
+	}
+	public Chiusura_anno_inventarioKey(Long pgChiusura, Integer anno, String tipoChiusura, String cdCategoriaGruppo) {
+		super();
+		this.pgChiusura=pgChiusura;
+		this.anno=anno;
+		this.tipoChiusura=tipoChiusura;
+		this.cdCategoriaGruppo=cdCategoriaGruppo;
+	}
+	public boolean equalsByPrimaryKey(Object o) {
+		if (this== o) return true;
+		if (!(o instanceof Chiusura_anno_inventarioKey)) return false;
+		Chiusura_anno_inventarioKey k = (Chiusura_anno_inventarioKey) o;
+		if (!compareKey(getPgChiusura(), k.getPgChiusura())) return false;
+		if (!compareKey(getAnno(), k.getAnno())) return false;
+		if (!compareKey(getTipoChiusura(), k.getTipoChiusura())) return false;
+		if (!compareKey(getCdCategoriaGruppo(), k.getCdCategoriaGruppo())) return false;
+		return true;
+	}
+	public int primaryKeyHashCode() {
+		int i = 0;
+		i = i + calculateKeyHashCode(getPgChiusura());
+		i = i + calculateKeyHashCode(getAnno());
+		i = i + calculateKeyHashCode(getTipoChiusura());
+		i = i + calculateKeyHashCode(getCdCategoriaGruppo());
+		return i;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Progressivo record chiusura]
+	 **/
+	public void setPgChiusura(Long pgChiusura)  {
+		this.pgChiusura=pgChiusura;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Progressivo record chiusura]
+	 **/
+	public Long getPgChiusura() {
+		return pgChiusura;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Anno di chiusura]
+	 **/
+	public void setAnno(Integer anno)  {
+		this.anno=anno;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Anno di chiusura]
+	 **/
+	public Integer getAnno() {
+		return anno;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [M=Magazzino; I=Inventario]
+	 **/
+	public void setTipoChiusura(String tipoChiusura)  {
+		this.tipoChiusura=tipoChiusura;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [M=Magazzino; I=Inventario]
+	 **/
+	public String getTipoChiusura() {
+		return tipoChiusura;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Restituisce il valore di: [Codice categoria/gruppo]
+	 **/
+	public void setCdCategoriaGruppo(String cdCategoriaGruppo)  {
+		this.cdCategoriaGruppo=cdCategoriaGruppo;
+	}
+	/**
+	 * Created by BulkGenerator 2.0 [07/12/2009]
+	 * Setta il valore di: [Codice categoria/gruppo]
+	 **/
+	public String getCdCategoriaGruppo() {
+		return cdCategoriaGruppo;
+	}
+}
