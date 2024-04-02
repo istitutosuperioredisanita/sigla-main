287b10d5fe0c7cc6d8b77e8077109c910aa6ebbe
salvio_ciro <cirosalvio@hotmail.com>
2024-04-02 11:49:10 +0200

    Aggiunto sulla vista l'informazione se un bene è migrato e il suo piano di ammortamento originario

--- sigla-backend/src/main/resources/expsigladb/View/V_INVENTARIO_BENE_DET.sql
+++ sigla-backend/src/main/resources/expsigladb/View/V_INVENTARIO_BENE_DET.sql
@@ -2,7 +2,7 @@
 --  DDL for View V_INVENTARIO_BENE_DET
 --------------------------------------------------------
 
-  CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_INVENTARIO_BENE_DET" ("TIPORECORD", "ESERCIZIO_CARICO_BENE", "PG_INVENTARIO", "NR_INVENTARIO", "ETICHETTA", "CD_CATEGORIA_GRUPPO", "VALORE_INIZIALE", "VALORE_AMMORTIZZATO", "VARIAZIONE_PIU", "VARIAZIONE_MENO", "ESERCIZIO_BUONO_CARICO", "INCREMENTO_VALORE", "DECREMENTO_VALORE", "PG_BUONO_C_S", "QUOTA_AMMO_BENE_ALIENATO", "ESERCIZIO_AMMORTANENTO", "QUOTA_AMMORTAMENTO") AS
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "SIGLACOPIAPROD"."V_INVENTARIO_BENE_DET" ("TIPORECORD", "ESERCIZIO_CARICO_BENE", "PG_INVENTARIO", "NR_INVENTARIO", "ETICHETTA", "FL_MIGRATO", "CD_CATEGORIA_GRUPPO", "VALORE_INIZIALE", "VALORE_AMMORTIZZATO", "VARIAZIONE_PIU", "VARIAZIONE_MENO", "ESERCIZIO_BUONO_CARICO", "INCREMENTO_VALORE", "DECREMENTO_VALORE", "PG_BUONO_C_S", "QUOTA_AMMO_BENE_ALIENATO", "ESERCIZIO_AMMORTANENTO", "QUOTA_AMMORTAMENTO", "PIANO_AMM_BENE_MIGRATO") AS
   SELECT
 --
 -- Date: 22/03/2024
@@ -19,13 +19,15 @@
                       a.pg_inventario,
                       a.nr_inventario,
                       a.etichetta,
+                       a.fl_migrato,
                       a.cd_categoria_gruppo,
                       a.valore_iniziale,
                       a.valore_ammortizzato,
                       NVL(a.variazione_piu,0)variazione_piu,
                       NVL(variazione_meno,0)variazione_meno,
                       null esercizio_buono_carico,
-                      0 incremento_valore,0 decremento_valore, null PG_BUONO_C_S,0 quota_ammo_bene_alienato,null esercizio_ammortanento, 0 quota_ammortamento
+                      0 incremento_valore,0 decremento_valore, null PG_BUONO_C_S,0 quota_ammo_bene_alienato,null esercizio_ammortanento, 0 quota_ammortamento,
+                      a.piano_amm_bene_migrato
               from inventario_beni a
                   union all
               select 'INCREMENTO' tipoRecord,
@@ -33,6 +35,7 @@
                       a.pg_inventario,
                       a.nr_inventario,
                       a.etichetta,
+                       a.fl_migrato,
                       a.cd_categoria_gruppo,
                       0 valore_iniziale ,
                       0 valore_ammortizzato,
@@ -44,7 +47,8 @@
                       d.PG_BUONO_C_S,
                       0 quota_ammo_bene_alienato,
                       null esercizio_ammortanento,
-                       0 quota_ammortamento
+                       0 quota_ammortamento,
+                        a.piano_amm_bene_migrato
               from inventario_beni a
                       left outer join buono_carico_scarico_dett d
                           on d.pg_inventario=a.pg_inventario
@@ -63,6 +67,7 @@
                       a.pg_inventario,
                       a.nr_inventario,
                       a.etichetta,
+                       a.fl_migrato,
                       a.cd_categoria_gruppo,
                       0 valore_iniziale ,
                       0 valore_ammortizzato,
@@ -74,7 +79,8 @@
                       d.PG_BUONO_C_S  PG_BUONO_C_S,
                       DECODE( t.cd_tipo_carico_scarico,'3',a.valore_ammortizzato, 0)  quota_ammo_bene_alienato,
                       null esercizio_ammortanento,
-                      0 quota_ammortamento
+                      0 quota_ammortamento,
+                      a.piano_amm_bene_migrato
               from inventario_beni a
                       left outer join buono_carico_scarico_dett d
                           on d.pg_inventario=a.pg_inventario
@@ -93,6 +99,7 @@
                       a.pg_inventario,
                       a.nr_inventario,
                       a.etichetta,
+                       a.fl_migrato,
                       a.cd_categoria_gruppo,
                       0 valore_iniziale ,
                       0 valore_ammortizzato,
@@ -104,7 +111,8 @@
                       null PG_BUONO_C_S,
                       0 quota_ammo_bene_alienato,
                       i.esercizio esercizio_ammortanento,
-                      i.im_movimento_ammort quota_ammortamento
+                      i.im_movimento_ammort quota_ammortamento,
+                       a.piano_amm_bene_migrato
               from inventario_beni a
                       inner join ammortamento_bene_inv i on
                       a.pg_inventario=i.pg_inventario
