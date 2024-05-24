377f86eecfa929fe0e3414c56e818a7e89108110
salvio_ciro <cirosalvio@hotmail.com>
2024-05-24 14:37:30 +0200

    implementazione autofattura

---
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/bp/CRUDAutofatturaBP.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.docamm00.bp;
+
+import it.cnr.jada.util.action.SimpleCRUDBP;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Gestisce le catene di elementi correlate con la fattura passiva in uso.
+ */
+public abstract class CRUDAutofatturaBP extends SimpleCRUDBP {
+
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(CRUDAutofatturaBP.class);
+
+
+}
---
+++ sigla-web/src/main/webapp/docamm00/autofattura.jsp
@@ -0,0 +1,22 @@
+<%@ page pageEncoding="UTF-8"
+	import="it.cnr.jada.util.jsp.*,
+		it.cnr.jada.action.*,
+		java.util.*,
+		it.cnr.jada.util.action.*,
+		it.cnr.contab.docamm00.tabrif.bulk.*,
+		it.cnr.contab.docamm00.docs.bulk.*,
+		it.cnr.contab.docamm00.bp.*"
+%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<% JSPUtils.printBaseUrl(pageContext); %>
+<script language="javascript" src="scripts/css.js"></script>
+<script language="JavaScript" src="scripts/util.js"></script>
+<% CRUDFatturaPassivaBP bp = (CRUDFatturaPassivaBP)BusinessProcess.getBusinessProcess(request); %>
+<title> Autofatture/title>
+
+</head>
+<body class="Form">
+Autofatture
+</body>
