db5fc00b0c7b58edd13c91bd08f1bf98ecd1e3e2
salvio_ciro <cirosalvio@hotmail.com>
2024-07-18 18:16:03 +0200

    Implementazione Consultazione Accertamento Scadenzario Voce

---
+++ sigla-backend/src/main/resources/expsigladb/View/V_CONS_SCAD_ACCERT_VOCE.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------
+--  DDL for View V_CONS_SCAD_ACCERT_VOCE
+--------------------------------------------------------
+
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_CONS_SCAD_ACCERT_VOCE" ("CDS", "UO", "ESERCIZIO", "ESERCIZIO_ORIGINALE", "PG_ACC", "PG_ACC_SCAD", "VOCE_BILANCIO", "DATA_SCAD", "ACC_DESC", "DS_SCAD", "IM_SCAD", "IM_ASS_DOC_AMM", "IMP_ASS_DOC_CONT", "DEBITORE", "CD_CDS_ACCERTAMENTO", "DS_ELEMENTO_VOCE", "PG_PROGETTO", "CD_PROGETTO","DS_PROGETTO", "DT_INIZIO", "DT_FINE", "DT_PROROGA", "CD_TIPO_PROGETTO", "DS_TIPO_PROGETTO") AS
+    SELECT
+  --
+  -- Version: 1.0
+  --
+  -- Vista per la consultazione Scadenzario accertamenti
+  --
+  --
+  -- Body
+  --
+     v."CDS",v."UO",v."ESERCIZIO",v."ESERCIZIO_ORIGINALE",v."PG_ACC",v."PG_ACC_SCAD",v."VOCE_BILANCIO",v."DATA_SCAD",v."ACC_DESC",v."DS_SCAD",v."IM_SCAD",v."IM_ASS_DOC_AMM",v."IMP_ASS_DOC_CONT",v."DEBITORE",v."CD_CDS_ACCERTAMENTO",v."DS_ELEMENTO_VOCE",p.pg_progetto,p.cd_progetto,p.ds_progetto,po.dt_inizio,po.dt_fine,po.dt_proroga,tp.cd_tipo_progetto,tp.ds_tipo_progetto from V_CONS_SCAD_ACCERT v
+     inner join accertamento_scad_voce av
+     on   v.cd_cds_accertamento= av.cd_cds
+         AND v.esercizio = av.esercizio
+         and v.esercizio_originale =av.esercizio_originale
+                    AND v.PG_ACC =av.pg_accertamento
+                    AND v.PG_ACC_SCAD = av.PG_ACCERTAMENTO_SCADENZARIO
+     inner join   v_linea_attivita_valida l
+     on av.esercizio=l.esercizio
+     and av.cd_linea_attivita=l.cd_linea_attivita
+     and  av.cd_centro_responsabilita=l.cd_centro_responsabilita
+     inner join progetto p
+     on p.esercizio=l.esercizio
+     and p.pg_progetto=l.pg_progetto
+     and p.tipo_fase = 'X'
+     inner join progetto_other_field po
+     on p.pg_progetto_other_field=po.pg_progetto
+     inner join tipo_progetto tp
+     on p.cd_tipo_progetto=tp.cd_tipo_progetto;
+
--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_scad_accertHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/V_cons_scad_accertHome.java
@@ -20,7 +20,6 @@
  * Date 09/03/2007
  */
 package it.cnr.contab.doccont00.consultazioni.bulk;
-import java.sql.Connection;
 
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
 import it.cnr.contab.config00.sto.bulk.Unita_organizzativa_enteBulk;
@@ -31,6 +30,8 @@ import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.sql.Connection;
 public class V_cons_scad_accertHome extends BulkHome {
 	public V_cons_scad_accertHome(Connection conn) {
 		super(V_cons_scad_accertBulk.class, conn);
@@ -38,7 +39,15 @@ public class V_cons_scad_accertHome extends BulkHome {
 	public V_cons_scad_accertHome(Connection conn, PersistentCache persistentCache) {
 		super(V_cons_scad_accertBulk.class, conn, persistentCache);
 	}
-	
+
+	protected V_cons_scad_accertHome(Class clazz, java.sql.Connection connection) {
+		super(clazz, connection);
+	}
+
+	protected V_cons_scad_accertHome(Class clazz, java.sql.Connection connection, PersistentCache persistentCache) {
+		super(clazz, connection, persistentCache);
+	}
+
 	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause) throws PersistencyException
 	{
 		SQLBuilder sql = super.selectByClause(usercontext, compoundfindclause);
