2e9a95d7b28740efcff2371331441ae851452130
salvio_ciro <cirosalvio@hotmail.com>
2024-08-09 15:26:25 +0200

    Implementazione Consultazione Impegni/Accertamenti Scadenzario Voce

--- sigla-backend/src/main/resources/expsigladb/View/V_CONS_SCAD_OBBL_VOCE.sql
+++ sigla-backend/src/main/resources/expsigladb/View/V_CONS_SCAD_OBBL_VOCE.sql
@@ -2,7 +2,7 @@
 --  DDL for View V_CONS_SCAD_OBBL
 --------------------------------------------------------
 
-  CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_CONS_SCAD_OBBL_VOCE" ("CDS", "UO", "ESERCIZIO", "ESERCIZIO_ORIGINALE", "PG_OBBL", "PG_OBBL_SCAD", "VOCE_BILANCIO", "DATA_SCAD", "DS_OBBL", "DS_SCAD", "IM_SCAD", "IM_ASS_DOC_AMM", "IMP_ASS_DOC_CONT", "CREDITORE", "DS_ELEMENTO_VOCE", "PG_PROGETTO", "CD_PROGETTO", "DS_PROGETTO", "DT_INIZIO", "DT_FINE", "DT_PROROGA", "CD_TIPO_PROGETTO", "DS_TIPO_PROGETTO") AS
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "V_CONS_SCAD_OBBL_VOCE" ("CDS", "UO", "ESERCIZIO", "ESERCIZIO_ORIGINALE", "PG_OBBL", "PG_OBBL_SCAD", "VOCE_BILANCIO", "DATA_SCAD", "DS_OBBL", "DS_SCAD", "IM_SCAD", "IM_ASS_DOC_AMM", "IMP_ASS_DOC_CONT", "CREDITORE", "DS_ELEMENTO_VOCE", "IM_VOCE","PG_PROGETTO", "CD_PROGETTO", "DS_PROGETTO", "DT_INIZIO", "DT_FINE", "DT_PROROGA", "CD_TIPO_PROGETTO", "DS_TIPO_PROGETTO") AS
   SELECT
 --
 -- Date: 07/03/2007
@@ -28,6 +28,7 @@ v.IM_ASS_DOC_AMM,
 v.IMP_ASS_DOC_CONT,
 v.CREDITORE,
 v.DS_ELEMENTO_VOCE,
+ov.im_voce,
 p.pg_progetto,
 p.cd_progetto,
 p.ds_progetto,
@@ -46,9 +47,13 @@ from v_cons_scad_obbl v
      inner join linea_attivita l
      on  ov.cd_linea_attivita=l.cd_linea_attivita
      and  ov.cd_centro_responsabilita=l.cd_centro_responsabilita
+     inner join ass_linea_attivita_esercizio ass
+             on ass.cd_linea_attivita=l.cd_linea_attivita
+             and ass.cd_centro_responsabilita=l.cd_centro_responsabilita
+             and ass.esercizio=v.esercizio
      inner join progetto p
-     on p.esercizio=v.esercizio
-     and p.pg_progetto=l.pg_progetto
+     on p.esercizio=ass.esercizio
+     and p.pg_progetto=ass.pg_progetto
      and p.tipo_fase = 'X'
      inner join progetto_other_field po
      on p.pg_progetto_other_field=po.pg_progetto
