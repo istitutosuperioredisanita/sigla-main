3a857fa4cc2dd350e4bffbd767c9290868048ee2
salvio_ciro <cirosalvio@hotmail.com>
2025-01-29 10:41:49 +0100

    Fix controllo pesenza data pagamento richiesta per mandato con modalità di pagamento diverso da F24EP

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/MandatoComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/MandatoComponent.java
@@ -5775,20 +5775,20 @@ public class MandatoComponent extends ScritturaPartitaDoppiaFromDocumentoCompone
                 if (rifModPag.isMandatoRegSospeso() && !mandato.isRegolamentoSospeso())
                     throw new ApplicationException(
                             "Attenzione per la modalità di pagamento indicata il mandato deve essere a regolamento sospeso.");
-                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") == 0 || rifModPag.getTipo_pagamento_siope().compareTo("F24EP")==0)
+                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") == 0 || ("F24EP").compareTo(rifModPag.getTipo_pagamento_siope())==0)
                             && mandato.getDt_pagamento_richiesta() == null)
                     throw new ApplicationException(
                             "Attenzione per la modalità di pagamento indicata il mandato deve avere la data pagamento richiesta.");
 
-                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") != 0 && !(rifModPag.getTipo_pagamento_siope().compareTo("F24EP")==0))
+                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") != 0 && !(("F24EP").compareTo(rifModPag.getTipo_pagamento_siope())==0))
                         && mandato.getDt_pagamento_richiesta() != null)
                     throw new ApplicationException(
                             "Attenzione per la modalità di pagamento " + rifModPag.getCd_modalita_pag() + " la data pagamento richiesta non deve essere indicata.");
-                if (( rifModPag.getTipo_pagamento_siope().compareTo("F24EP") != 0 && !(rifModPag.getCd_modalita_pag().compareTo("F24EP")==0))
+                if (( ("F24EP").compareTo(rifModPag.getTipo_pagamento_siope()) != 0 && !(("F24EP").compareTo(rifModPag.getCd_modalita_pag())==0))
                         && mandato.getDt_pagamento_richiesta() != null)
                     throw new ApplicationException(
                             "Attenzione per la modalità di pagamento " + rifModPag.getCd_modalita_pag() + " la data pagamento richiesta non deve essere indicata.");
-                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") == 0 || rifModPag.getTipo_pagamento_siope().compareTo("F24EP")==0)
+                if (( rifModPag.getCd_modalita_pag().compareTo("F24EP") == 0 || ("F24EP").compareTo(rifModPag.getTipo_pagamento_siope())==0)
                         && mandato.getDt_pagamento_richiesta() != null &&
                         mandato.getDt_emissione() != null && mandato.getDt_pagamento_richiesta().before(mandato.getDt_emissione()))
                     throw new ApplicationException(
