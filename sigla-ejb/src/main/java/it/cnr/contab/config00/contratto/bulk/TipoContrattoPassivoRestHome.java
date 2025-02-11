1220a480cdc395b1c735e929f3253003722cca08
salvio_ciro <cirosalvio@hotmail.com>
2025-02-11 19:28:10 +0100

    Implementazione Rest per tipologiche contratto

---
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/contratto/bulk/TipoContrattoPassivoRestHome.java
@@ -0,0 +1,34 @@
+package it.cnr.contab.config00.contratto.bulk;
+
+import it.cnr.contab.consultazioni.bulk.ConsultazioniRestHome;
+import it.cnr.jada.UserContext;
+import it.cnr.jada.bulk.OggettoBulk;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
+import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
+public class TipoContrattoPassivoRestHome extends Tipo_contrattoHome implements ConsultazioniRestHome {
+    public TipoContrattoPassivoRestHome(java.sql.Connection conn) {
+        super(TipoContrattoPassivoRestBulk.class, conn);
+    }
+    public TipoContrattoPassivoRestHome(java.sql.Connection conn, PersistentCache persistentCache) {
+        super(TipoContrattoPassivoRestBulk.class, conn, persistentCache);
+    }
+
+
+    @Override
+    public SQLBuilder restSelect(UserContext userContext, SQLBuilder sql, CompoundFindClause compoundfindclause, OggettoBulk oggettobulk) throws ComponentException, PersistencyException {
+
+        sql.addClause("AND", "fl_cancellato", SQLBuilder.EQUALS, Boolean.FALSE);
+       sql.openParenthesis("AND");
+        sql.addClause("OR", "natura_contabile", SQLBuilder.EQUALS, Tipo_contrattoBulk.NATURA_CONTABILE_PASSIVO);
+        sql.addClause("OR", "natura_contabile", SQLBuilder.EQUALS, Tipo_contrattoBulk.NATURA_CONTABILE_SENZA_FLUSSI_FINANZIARI);
+        sql.closeParenthesis();
+
+
+        return sql;
+
+    }
+}
