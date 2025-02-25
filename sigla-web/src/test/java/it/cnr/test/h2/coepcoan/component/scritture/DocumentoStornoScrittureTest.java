208025986a2a2078859dffd2e484121f9a764e40
raffaele.pagano <raffaele.pagano@cnr.it>
2025-02-25 23:03:09 +0100

    Test per controllo scritture contabili: implementazione nuovi test

--- sigla-web/src/test/java/it/cnr/test/h2/coepcoan/component/scritture/DocumentoStornoScrittureTest.java
+++ sigla-web/src/test/java/it/cnr/test/h2/coepcoan/component/scritture/DocumentoStornoScrittureTest.java
@@ -152,4 +152,255 @@ public class DocumentoStornoScrittureTest extends DeploymentsH2 {
             Utility.createScritturaPartitaDoppiaFromDocumentoComponentSession().modificaConBulk(new TestUserContext(), documentoCogeBulk);
         }
     }
+
+
+    /**
+     * {@code 2 Documenti Generici Stornati} con un unico {@code Documento di Storno}.
+     * <p><b>Dati Primo Documento Generico (A)</b>
+     * <pre>
+     *     <b>Riga 1A</b>
+     *        Voce Bilancio: 22011 - Attrezzature sanitarie
+     *        Importo:       30,00
+     *     <b>Riga 2A</b>
+     *        Voce Bilancio: 22010 - Attrezzature scientifiche
+     *        Importo:       70,00
+     * </pre></p>
+     * <p><b>Dati Secondo Documento Generico (B)</b>
+     * <pre>
+     *     <b>Riga 1B</b>
+     *        Voce Bilancio: 22011 - Attrezzature sanitarie
+     *        Importo:      100,00
+     * </pre></p>
+     * <p><b>Dati Primo Documento Generico di Storno (C)</b>
+     * <pre>
+     *     <b>Riga 1C - storna riga 2A</b>
+     *        Voce Bilancio: 22010 - Attrezzature scientifiche
+     *        Importo:       70,00
+     *     <b>Riga 2C - storna riga 1B</b>
+     *        Voce Bilancio: 22011 - Attrezzature sanitarie
+     *        Importo:      100,00
+     * </pre></p>
+     * <p><b>Dati Secondo Documento Generico di Storno (D)</b>
+     * <pre>
+     *     <b>Riga 1D - storna riga 1A</b>
+     *        Voce Bilancio: 22011 - Attrezzature sanitarie
+     *        Importo:       30,00
+     * </pre></p>
+     * <b>Scrittura Economica Primo Documento Generico</b>
+     * <pre>
+     *     Sezione   Importo      Conto
+     *        D        30,00      A22011 - Attrezzature sanitarie
+     *        D        70,00      A22010 - Attrezzature scientifiche
+     *        A        30,00      P22011 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature sanitarie
+     *        A        70,00      P22010 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature scientifiche
+     * </pre>
+     * <b>Scrittura Economica Secondo Documento Generico</b>
+     * <pre>
+     *     Sezione   Importo      Conto
+     *        D       100,00      A22011 - Attrezzature sanitarie
+     *        A       100,00      P22011 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature sanitarie
+     * </pre>
+     * <b>Scrittura Economica Primo Documento di Storno</b>
+     * <pre>
+     *     Sezione   Importo      Conto
+     *        D        70,00      P22010 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature scientifiche
+     *        D       100,00      P22011 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature sanitarie
+     *        A        70,00      A22010 - Attrezzature scientifiche
+     *        A       100,00      A22011 - Attrezzature sanitarie
+     * </pre>
+     * <b>Scrittura Economica Secondo Documento di Storno</b>
+     * <pre>
+     *     Sezione   Importo      Conto
+     *        D        30,00      P22011 - Debiti verso fornitori per acquisto di
+     *                                     attrezzature sanitarie
+     *        A        30,00      A22011 - Attrezzature sanitarie
+     * </pre>
+     */
+    @Test
+    @OperateOnDeployment(TEST_H2)
+    @InSequence(2)
+    public void testDocumentoGenericoStorno002() throws Exception {
+        {
+            Documento_genericoBulk documentoCogeBulk = Optional.ofNullable(crudComponentSession.findByPrimaryKey(new TestUserContext(),
+                            new Documento_genericoBulk("000", "GENERICO_S", "000.000", 2025,
+                                    5L)))
+                    .filter(Documento_genericoBulk.class::isInstance)
+                    .map(Documento_genericoBulk.class::cast)
+                    .orElse(null);
+            ResultScrittureContabili result = Utility.createProposeScritturaComponentSession().proposeScrittureContabili(
+                    new TestUserContext(),
+                    documentoCogeBulk);
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getIm_scrittura).orElse(null));
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleDare).orElse(null));
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleAvere).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiDare = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiDareColl)
+                    .orElse(new BulkList<>());
+            assertEquals(2, movimentiDare.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita1 = movimentiDare.stream().filter(el->"A22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22011 non presente.", rigaTipoAttivita1.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita1.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("30.00"), rigaTipoAttivita1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita2 = movimentiDare.stream().filter(el->"A22010".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22010 non presente.", rigaTipoAttivita2.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita2.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("70.00"), rigaTipoAttivita2.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiAvere = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiAvereColl)
+                    .orElse(new BulkList<>());
+            assertEquals(2, movimentiAvere.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito1 = movimentiAvere.stream().filter(el->"P22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22011 non presente.", rigaTipoDebito1.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito1.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("30.00"), rigaTipoDebito1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito2 = movimentiAvere.stream().filter(el->"P22010".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22010 non presente.", rigaTipoDebito2.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito2.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("70.00"), rigaTipoDebito2.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Utility.createScritturaPartitaDoppiaFromDocumentoComponentSession().modificaConBulk(new TestUserContext(), documentoCogeBulk);
+        }
+        {
+            Documento_genericoBulk documentoCogeBulk = Optional.ofNullable(crudComponentSession.findByPrimaryKey(new TestUserContext(),
+                            new Documento_genericoBulk("000", "GENERICO_S", "000.000", 2025,
+                                    6L)))
+                    .filter(Documento_genericoBulk.class::isInstance)
+                    .map(Documento_genericoBulk.class::cast)
+                    .orElse(null);
+            ResultScrittureContabili result = Utility.createProposeScritturaComponentSession().proposeScrittureContabili(
+                    new TestUserContext(),
+                    documentoCogeBulk);
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getIm_scrittura).orElse(null));
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleDare).orElse(null));
+            assertEquals(new BigDecimal("100.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleAvere).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiDare = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiDareColl)
+                    .orElse(new BulkList<>());
+            assertEquals(1, movimentiDare.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita1 = movimentiDare.stream().filter(el->"A22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22011 non presente.", rigaTipoAttivita1.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita1.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("100.00"), rigaTipoAttivita1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiAvere = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiAvereColl)
+                    .orElse(new BulkList<>());
+            assertEquals(1, movimentiAvere.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito1 = movimentiAvere.stream().filter(el->"P22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22011 non presente.", rigaTipoDebito1.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito1.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("100.00"), rigaTipoDebito1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Utility.createScritturaPartitaDoppiaFromDocumentoComponentSession().modificaConBulk(new TestUserContext(), documentoCogeBulk);
+        }
+        {
+            Documento_genericoBulk documentoCogeBulk = Optional.ofNullable(crudComponentSession.findByPrimaryKey(new TestUserContext(),
+                            new Documento_genericoBulk("000", "GENERICO_S", "000.000", 2025,
+                                    7L)))
+                    .filter(Documento_genericoBulk.class::isInstance)
+                    .map(Documento_genericoBulk.class::cast)
+                    .orElse(null);
+            ResultScrittureContabili result = Utility.createProposeScritturaComponentSession().proposeScrittureContabili(
+                    new TestUserContext(),
+                    documentoCogeBulk);
+            assertEquals(new BigDecimal("170.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getIm_scrittura).orElse(null));
+            assertEquals(new BigDecimal("170.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleDare).orElse(null));
+            assertEquals(new BigDecimal("170.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleAvere).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiAvere = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiAvereColl)
+                    .orElse(new BulkList<>());
+            assertEquals(2, movimentiAvere.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita1 = movimentiAvere.stream().filter(el->"A22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22011 non presente.", rigaTipoAttivita1.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita1.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("100.00"), rigaTipoAttivita1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita2 = movimentiAvere.stream().filter(el->"A22010".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22010 non presente.", rigaTipoAttivita2.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita2.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("70.00"), rigaTipoAttivita2.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiDare = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiDareColl)
+                    .orElse(new BulkList<>());
+            assertEquals(2, movimentiDare.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito1 = movimentiDare.stream().filter(el->"P22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22011 non presente.", rigaTipoDebito1.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito1.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("100.00"), rigaTipoDebito1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito2 = movimentiDare.stream().filter(el->"P22010".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22010 non presente.", rigaTipoDebito2.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito2.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("70.00"), rigaTipoDebito2.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Utility.createScritturaPartitaDoppiaFromDocumentoComponentSession().modificaConBulk(new TestUserContext(), documentoCogeBulk);
+        }
+        {
+            Documento_genericoBulk documentoCogeBulk = Optional.ofNullable(crudComponentSession.findByPrimaryKey(new TestUserContext(),
+                            new Documento_genericoBulk("000", "GENERICO_S", "000.000", 2025,
+                                    8L)))
+                    .filter(Documento_genericoBulk.class::isInstance)
+                    .map(Documento_genericoBulk.class::cast)
+                    .orElse(null);
+            ResultScrittureContabili result = Utility.createProposeScritturaComponentSession().proposeScrittureContabili(
+                    new TestUserContext(),
+                    documentoCogeBulk);
+            assertEquals(new BigDecimal("30.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getIm_scrittura).orElse(null));
+            assertEquals(new BigDecimal("30.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleDare).orElse(null));
+            assertEquals(new BigDecimal("30.00"), Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getImTotaleAvere).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiAvere = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiAvereColl)
+                    .orElse(new BulkList<>());
+            assertEquals(1, movimentiAvere.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoAttivita1 = movimentiAvere.stream().filter(el->"A22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto A22011 non presente.", rigaTipoAttivita1.isPresent());
+            assertTrue("Riga tipo attività non presente.", rigaTipoAttivita1.filter(Movimento_cogeBulk::isRigaTipoAttivita).isPresent());
+            assertEquals(new BigDecimal("30.00"), rigaTipoAttivita1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            BulkList<Movimento_cogeBulk> movimentiDare = Optional.ofNullable(result.getScritturaPartitaDoppiaBulk())
+                    .map(Scrittura_partita_doppiaBulk::getMovimentiDareColl)
+                    .orElse(new BulkList<>());
+            assertEquals(1, movimentiDare.size());
+
+            Optional<Movimento_cogeBulk> rigaTipoDebito1 = movimentiDare.stream().filter(el->"P22011".equals(el.getCd_voce_ep())).findAny();
+            assertTrue("Conto P22011 non presente.", rigaTipoDebito1.isPresent());
+            assertTrue("Riga tipo debito non presente.", rigaTipoDebito1.filter(Movimento_cogeBulk::isRigaTipoDebito).isPresent());
+            assertEquals(new BigDecimal("30.00"), rigaTipoDebito1.map(Movimento_cogeBulk::getIm_movimento).orElse(null));
+
+            Utility.createScritturaPartitaDoppiaFromDocumentoComponentSession().modificaConBulk(new TestUserContext(), documentoCogeBulk);
+        }
+    }
+
 }
