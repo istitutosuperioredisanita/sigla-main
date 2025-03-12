11bfa1a7da60168d2026cfa4ab7f5da0f377fd0d
marco.spasiano <marco.spasiano@cnr.it>
2025-03-12 16:49:09 +0100

    fix: Mappa Associazione Conto Gruppo, la Voce Economico/Patrimoniale non veniva valorizzata correttamente

--- sigla-ejb/src/main/java/it/cnr/contab/coepcoan00/action/CRUDAssociazioneContoGruppoAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/coepcoan00/action/CRUDAssociazioneContoGruppoAction.java
@@ -1,17 +1,14 @@
 package it.cnr.contab.coepcoan00.action;
 
-import it.cnr.contab.anagraf00.core.bulk.TerzoBulk;
 import it.cnr.contab.coepcoan00.bp.CRUDAssociazioneContoGruppoBP;
 import it.cnr.contab.config00.pdcep.bulk.AssociazioneContoGruppoBulk;
-import it.cnr.contab.config00.pdcep.bulk.Voce_epBulk;
-import it.cnr.contab.docamm00.fatturapa.bulk.DocumentoEleTestataBulk;
+import it.cnr.contab.config00.pdcep.bulk.ContoBulk;
 import it.cnr.jada.DetailedRuntimeException;
 import it.cnr.jada.action.ActionContext;
 import it.cnr.jada.action.BusinessProcessException;
 import it.cnr.jada.action.Forward;
 import it.cnr.jada.action.HookForward;
 import it.cnr.jada.bulk.FillException;
-import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.comp.ComponentException;
 import it.cnr.jada.util.RemoteIterator;
 import it.cnr.jada.util.action.CRUDAction;
@@ -37,14 +34,14 @@ public class CRUDAssociazioneContoGruppoAction extends CRUDAction {
                 return actioncontext.findDefaultForward();
             }
             if (remoteiterator.countElements() == 1) {
-                Voce_epBulk oggettobulk1 = (Voce_epBulk) remoteiterator.nextElement();
+                ContoBulk oggettobulk1 = (ContoBulk) remoteiterator.nextElement();
                 EJBCommonServices.closeRemoteIterator(actioncontext, remoteiterator);
                 bp.setMessage(FormBP.INFO_MESSAGE, "La ricerca ha fornito un solo risultato.");
                 return doSelezionaConto(actioncontext, oggettobulk1);
             } else {
                 SelezionatoreListaBP selezionatorelistabp = (SelezionatoreListaBP) actioncontext.createBusinessProcess("Selezionatore");
                 selezionatorelistabp.setIterator(actioncontext, remoteiterator);
-                selezionatorelistabp.setBulkInfo(it.cnr.jada.bulk.BulkInfo.getBulkInfo(Voce_epBulk.class));
+                selezionatorelistabp.setBulkInfo(it.cnr.jada.bulk.BulkInfo.getBulkInfo(ContoBulk.class));
                 selezionatorelistabp.setFormField(bp.getFormField("voceEp"));
                 actioncontext.addHookForward("seleziona", this, "doSelezionaConto");
                 return actioncontext.addBusinessProcess(selezionatorelistabp);
@@ -94,14 +91,14 @@ public class CRUDAssociazioneContoGruppoAction extends CRUDAction {
 
     public Forward doSelezionaConto(ActionContext actioncontext) throws BusinessProcessException {
         HookForward caller = (HookForward) actioncontext.getCaller();
-        Optional<Voce_epBulk> voceEpBulk = Optional.ofNullable(caller.getParameter("selectedElements"))
+        Optional<ContoBulk> voceEpBulk = Optional.ofNullable(caller.getParameter("selectedElements"))
                 .filter(List.class::isInstance)
                 .map(List.class::cast)
                 .filter(list -> !list.isEmpty())
                 .map(list ->
                         list.stream()
-                                .filter(Voce_epBulk.class::isInstance)
-                                .map(Voce_epBulk.class::cast)
+                                .filter(ContoBulk.class::isInstance)
+                                .map(ContoBulk.class::cast)
                 ).orElse(Stream.empty())
                 .findAny();
         if (voceEpBulk.isPresent()) {
@@ -109,7 +106,8 @@ public class CRUDAssociazioneContoGruppoAction extends CRUDAction {
         }
         return actioncontext.findDefaultForward();
     }
-    public Forward doSelezionaConto(ActionContext actioncontext, Voce_epBulk voceEp) throws BusinessProcessException {
+
+    public Forward doSelezionaConto(ActionContext actioncontext, ContoBulk voceEp) throws BusinessProcessException {
         Optional.ofNullable(voceEp)
                 .ifPresent(voceEpBulk -> {
                     try {
