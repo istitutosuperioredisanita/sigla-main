fa396ecbea46edda47e1ae648f2661da8b6159b8
mspasiano <marco.spasiano@gmail.com>
2025-05-22 18:45:55 +0200

    Aggiunte informazioni ulteriori alla vista per i controlli PCC, abilitato il filtro per UO in scrivania

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/VControlliPCCHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/VControlliPCCHome.java
@@ -4,13 +4,37 @@
  */
 package it.cnr.contab.doccont00.consultazioni.bulk;
 import java.sql.Connection;
+
+import it.cnr.contab.config00.sto.bulk.Unita_organizzativa_enteBulk;
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.FindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
 public class VControlliPCCHome extends BulkHome {
 	public VControlliPCCHome(Connection conn) {
 		super(VControlliPCCBulk.class, conn);
 	}
 	public VControlliPCCHome(Connection conn, PersistentCache persistentCache) {
 		super(VControlliPCCBulk.class, conn, persistentCache);
+	}
+
+	@Override
+	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause) throws PersistencyException {
+		SQLBuilder sqlBuilder = super.selectByClause(usercontext, compoundfindclause);
+		//	Se uo 999.000 in scrivania: visualizza tutto l'elenco
+		Unita_organizzativa_enteBulk ente = (Unita_organizzativa_enteBulk) getHomeCache().getHome(Unita_organizzativa_enteBulk.class).findAll().get(0);
+		if (!((CNRUserContext) usercontext).getCd_unita_organizzativa().equals( ente.getCd_unita_organizzativa())){
+			sqlBuilder.openParenthesis(FindClause.AND);
+				sqlBuilder.addClause(FindClause.AND,"cdUoCUU",SQLBuilder.ISNULL,null);
+				sqlBuilder.addClause(FindClause.AND,"cdUnitaOrganizzativa",SQLBuilder.EQUALS,CNRUserContext.getCd_unita_organizzativa(usercontext));
+			sqlBuilder.closeParenthesis();
+			sqlBuilder.addClause(FindClause.OR,"cdUoCUU",SQLBuilder.EQUALS,CNRUserContext.getCd_unita_organizzativa(usercontext));
+		}
+		return sqlBuilder;
 	}
 }
