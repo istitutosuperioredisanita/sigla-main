97d7a1279fa006ba8c943d9e5a09c360e8420625
salvio_ciro <cirosalvio@hotmail.com>
2025-06-17 14:52:14 +0200

    Consultazione confronta costi coge con cofi

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/V_confronta_costi_coge_cofiHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/consultazioni/bulk/V_confronta_costi_coge_cofiHome.java
@@ -4,8 +4,13 @@
  */
 package it.cnr.contab.docamm00.consultazioni.bulk;
 
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
 
 import java.sql.Connection;
 public class V_confronta_costi_coge_cofiHome extends BulkHome {
@@ -14,5 +19,12 @@ public class V_confronta_costi_coge_cofiHome extends BulkHome {
 	}
 	public V_confronta_costi_coge_cofiHome(Connection conn, PersistentCache persistentCache) {
 		super(V_confronta_costi_coge_cofiBulk.class, conn, persistentCache);
+	}
+	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause)
+			throws PersistencyException
+	{
+		SQLBuilder sql = super.selectByClause(usercontext, compoundfindclause);
+		sql.addSQLClause("AND","esercizio",SQLBuilder.EQUALS, CNRUserContext.getEsercizio(usercontext));
+		return sql;
 	}
 }
