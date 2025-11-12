e991ef73c4910afa7b70ef6129294c75485bd277
salvio_ciro <cirosalvio@hotmail.com>
2025-11-12 09:43:21 +0100

    Sviluppo flussi di cassa

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/FlussiDiCassaDtoHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/consultazioni/bulk/FlussiDiCassaDtoHome.java
@@ -37,19 +37,22 @@ public class FlussiDiCassaDtoHome extends BulkHome {
 
 	public SQLBuilder findFlussiDiCassa(UserContext uc, FlussiDiCassaDtoBulk flussi){
 		SQLBuilder sqlInterna = null;
+		String prefixClass=null;
 
-
-		if(flussi.getTipoFlusso().equals(FlussiDiCassaDtoBulk.REVERSALI))
+		if(flussi.getTipoFlusso().equals(FlussiDiCassaDtoBulk.REVERSALI)) {
+			prefixClass="'E.'";
 			this.setColumnMap("FLUSSO_REVERSALI");
-		else
+		}else {
 			this.setColumnMap("FLUSSO_MANDATI");
+			prefixClass="'S.'";
+		}
 
 		sqlInterna=this.createSQLBuilder();
 		sqlInterna.resetColumns();
 
 		sqlInterna.setHeader("SELECT " +
-"CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO1,'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO2),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO3),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO4),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO5),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO5) CLASSIFICAZIONE , " +
-				      "CLASSIFICAZIONE_VOCI_EP.DS_CLASSIFICAZIONE , " +
+"V_CLASSIFICAZIONE_VOCI_EP.CD_CLASSIFICAZIONE_5_LIV CLASSIFICAZIONE , " +
+				      "V_CLASSIFICAZIONE_VOCI_EP.DS_CLASSIFICAZIONE , " +
 					  " sum(CASE "+
 						" WHEN TO_CHAR("+ sqlInterna.getColumnMap().getTableName().concat(".DT_EMISSIONE").concat(",'mm') <=3 THEN IMPORTO ")+
 					    " ELSE 0 "+
@@ -72,16 +75,14 @@ public class FlussiDiCassaDtoHome extends BulkHome {
 		sqlInterna.addSQLJoin("CODICI_SIOPE.ESERCIZIO", sqlInterna.getColumnMap().getTableName().concat(".ESERCIZIO_SIOPE"));
 		sqlInterna.addSQLJoin("CODICI_SIOPE.CD_SIOPE", sqlInterna.getColumnMap().getTableName().concat(".CD_SIOPE"));
 
-		sqlInterna.addTableToHeader("CLASSIFICAZIONE_VOCI_EP");
-		sqlInterna.addSQLJoin("CLASSIFICAZIONE_VOCI_EP.ID_CLASSIFICAZIONE", "CODICI_SIOPE.ID_CLASSIFICAZIONE_SIOPE");
-		sqlInterna.addSQLClause(FindClause.AND, "CLASSIFICAZIONE_VOCI_EP.TIPO", SQLBuilder.EQUALS, "SP1");
+		sqlInterna.addTableToHeader("V_CLASSIFICAZIONE_VOCI_EP");
+		sqlInterna.addSQLJoin("V_CLASSIFICAZIONE_VOCI_EP.ID_CLASSIFICAZIONE", "CODICI_SIOPE.ID_CLASSIFICAZIONE_SIOPE");
+		sqlInterna.addSQLClause(FindClause.AND, "V_CLASSIFICAZIONE_VOCI_EP.TIPO", SQLBuilder.EQUALS, "SP1");
 
 		sqlInterna.addSQLClause(FindClause.AND, sqlInterna.getColumnMap().getTableName().concat(".ESERCIZIO"), SQLBuilder.EQUALS, flussi.getEsercizio());
 		sqlInterna.addSQLClause(FindClause.AND,  sqlInterna.getColumnMap().getTableName().concat(".DT_EMISSIONE"), SQLBuilder.GREATER_EQUALS, flussi.getDtEmissioneDa());
 		sqlInterna.addSQLClause(FindClause.AND,  sqlInterna.getColumnMap().getTableName().concat(".DT_EMISSIONE"), SQLBuilder.LESS_EQUALS, flussi.getDtEmissioneA());
-		sqlInterna.addSQLGroupBy("CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO1,'.')," +
-				"CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO2),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO3),'.')," +
-				"CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO4),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO5),'.'),CLASSIFICAZIONE_VOCI_EP.CD_LIVELLO5), CLASSIFICAZIONE_VOCI_EP.DS_CLASSIFICAZIONE ");
+		sqlInterna.addSQLGroupBy("V_CLASSIFICAZIONE_VOCI_EP.CD_CLASSIFICAZIONE_5_LIV, V_CLASSIFICAZIONE_VOCI_EP.DS_CLASSIFICAZIONE ");
 		return sqlInterna;
 	}
 
