f37c9bb990101b592c51e87b55b197b9148ee4c3
salvio_ciro <cirosalvio@hotmail.com>
2025-11-13 12:32:23 +0100

    Fix ricerca numeratori: Adesso la ricerca ritorna solo i numeratori di unita operative associate all'unità organizzativa del contesto

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/NumerazioneOrdHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/anag00/NumerazioneOrdHome.java
@@ -20,20 +20,20 @@
  * Date 26/04/2017
  */
 package it.cnr.contab.ordmag.anag00;
-import java.rmi.RemoteException;
-import java.sql.Connection;
-
-import javax.ejb.EJBException;
 
 import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
-import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 import it.cnr.jada.persistency.sql.FindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import javax.ejb.EJBException;
+import java.rmi.RemoteException;
+import java.sql.Connection;
 public class NumerazioneOrdHome extends BulkHome {
 	public NumerazioneOrdHome(Connection conn) {
 		super(NumerazioneOrdBulk.class, conn);
@@ -54,7 +54,9 @@ public class NumerazioneOrdHome extends BulkHome {
 			CompoundFindClause compoundfindclause) throws PersistencyException {
 		SQLBuilder sql = super.selectByClause(userContext, compoundfindclause);
 		sql.addSQLClause("AND","ESERCIZIO",SQLBuilder.EQUALS,CNRUserContext.getEsercizio(userContext));
-
+			sql.addTableToHeader("UNITA_OPERATIVA_ORD");
+			sql.addSQLJoin("NUMERAZIONE_ORD.CD_UNITA_OPERATIVA", "UNITA_OPERATIVA_ORD.CD_UNITA_OPERATIVA");
+			sql.addSQLClause(FindClause.AND,"UNITA_OPERATIVA_ORD.CD_UNITA_ORGANIZZATIVA",SQLBuilder.EQUALS,CNRUserContext.getCd_unita_organizzativa(userContext));
 		return sql;
 	}
 }
