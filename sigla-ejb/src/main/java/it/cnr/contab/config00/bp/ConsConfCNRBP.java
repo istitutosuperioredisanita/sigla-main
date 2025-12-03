4766581a059c73860433a92e8c583461281cf1a3
maxitiss <massimiliano.digregorio@iss.it>
2025-12-03 12:59:41 +0100

    Preparazione files di progetto per consultazione conf CNR

---
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/bp/ConsConfCNRBP.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.config00.bp;
+
+import it.cnr.contab.config00.sto.bulk.Unita_organizzativaBulk;
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.jada.action.ActionContext;
+import it.cnr.jada.action.BusinessProcessException;
+import it.cnr.jada.comp.ApplicationException;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
+public class ConsConfCNRBP extends it.cnr.jada.util.action.ConsultazioniBP {
+public ConsConfCNRBP() {
+	super();
+}
+protected void init(it.cnr.jada.action.Config config, ActionContext context) throws BusinessProcessException {
+	try { 
+
+		String cds = CNRUserContext.getCd_cds(context.getUserContext());
+		Integer esercizio = CNRUserContext.getEsercizio(context.getUserContext());
+
+		CompoundFindClause clauses = new CompoundFindClause(); 
+		if(!isUoEnte(context))	 {				
+		  clauses.addClause("AND", "cds", SQLBuilder.EQUALS, cds);
+	     }
+		else throw new ApplicationException("Consultazione non disponibile da UO ente!");
+		//clauses.addClause("AND", "esercizioContratto", SQLBuilder.EQUALS, esercizio);
+		setBaseclause(clauses);
+		super.init(config,context);
+				
+	}catch(Throwable e) { 
+		throw new BusinessProcessException(e);
+	}
+}
+public boolean isUoEnte(ActionContext context){	
+	Unita_organizzativaBulk uo = it.cnr.contab.utenze00.bulk.CNRUserInfo.getUnita_organizzativa(context);
+	if (uo.getCd_tipo_unita().equals(it.cnr.contab.config00.sto.bulk.Tipo_unita_organizzativaHome.TIPO_UO_ENTE))
+		return true;	
+	return false; 
+}
+}
