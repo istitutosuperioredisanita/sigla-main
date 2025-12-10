65a5170553e30ca679460b746112b4694fe57312
Lucattini Stefano <stefano.lucattini@iss.it>
2025-12-10 12:56:23 +0100

    Configurazione CNR
    Nuova voce per la gestione delle variabili della tabella

---
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/bp/GestConfCNRBP.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.config00.bp;
+
+import it.cnr.jada.action.ActionContext;
+import it.cnr.jada.action.BusinessProcessException;
+
+public class GestConfCNRBP extends it.cnr.jada.util.action.ConsultazioniBP {
+public GestConfCNRBP() {
+	super();
+}
+protected void init(it.cnr.jada.action.Config config, ActionContext context) throws BusinessProcessException {
+	try {
+		super.init(config,context);
+
+        setFreeSearchSet("CONS_CONF_TOTALE");
+		setColumns(getBulkInfo().getColumnFieldPropertyDictionary("CONS_CONF_TOTALE"));
+	}catch(Throwable e) { 
+		throw new BusinessProcessException(e);
+	}
+}
+
+}
