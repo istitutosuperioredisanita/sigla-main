efa61bd0a2f1886d17d3d2f933df7b455d9af214
mspasiano <marco.spasiano@gmail.com>
2025-12-11 08:59:41 +0100

    fix: test path and add suite

--- sigla-web/src/test/java/it/cnr/test/h2/ordmag/ordini/bp/CRUDOrdineAcqBP001SuiteTest.java
+++ sigla-web/src/test/java/it/cnr/test/h2/ordmag/ordini/bp/CRUDOrdineAcqBP001SuiteTest.java
@@ -1,13 +1,16 @@
 package it.cnr.test.h2.ordmag.ordini.bp;
 
-import org.junit.platform.engine.support.ReportEntrysupport.sui runner.RunWith;
-import org.junit.runner.*;
-@Sui
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
         CRUDOrdineAcqBP001Test_IT.class,
         CRUDOrdineAcqBP002Test_IT.class,
         CRUDOrdineAcqBP003Test_IT.class,
         CRUDOrdineAcqBP004Test_IT.class
 })
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public class CRUDOrdineAcqBP001SuiteTest { }
