8262c186fe06b66f007dbc6540e91163ff22f12e
Lucattini Stefano <stefano.lucattini@iss.it>
2026-01-21 13:19:01 +0100

    Nuova stampa accertamenti pluriennali voce

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/AccertPluVociBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/AccertPluVociBP.java
@@ -1,25 +1,14 @@
 package it.cnr.contab.doccont00.bp;
-import it.cnr.contab.doccont00.core.bulk.Obbligazione_pluriennale_voceBulk;
-import it.cnr.contab.doccont00.core.bulk.Obbligazione_pluriennale_voceHome;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.jada.action.ActionContext;
 import it.cnr.jada.action.BusinessProcessException;
-import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
-import it.cnr.jada.util.Config;
-import it.cnr.jada.util.RemoteIterator;
 import it.cnr.jada.util.action.ConsultazioniBP;
-import it.cnr.jada.util.jsp.Button;
-import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
-import java.util.Vector;
-import java.util.logging.Logger;
+public class AccertPluVociBP extends ConsultazioniBP{
 
-public class ObblPluVociBP extends ConsultazioniBP{
-
-    protected void init(it.cnr.jada.action.Config config, it.cnr.jada.action.ActionContext context)
+    protected void init(it.cnr.jada.action.Config config, ActionContext context)
             throws BusinessProcessException {
         try {
             CompoundFindClause clauses = new CompoundFindClause();
