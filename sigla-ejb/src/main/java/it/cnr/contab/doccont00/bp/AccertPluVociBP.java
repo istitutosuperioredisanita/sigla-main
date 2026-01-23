3a5c0a2bcbe024c612fb86ac013a32389efe94a7
massimiliano.digregorio <massimiliano.digregorio@iss.it>
2026-01-23 11:28:38 +0100

    Impegni pluriennali/accertamenti - mod filtro

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/AccertPluVociBP.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/bp/AccertPluVociBP.java
@@ -14,7 +14,6 @@ public class AccertPluVociBP extends ConsultazioniBP{
             CompoundFindClause clauses = new CompoundFindClause();
             int annoSelezionato = CNRUserContext.getEsercizio(context.getUserContext()).intValue();
             clauses.addClause("AND","esercizio", SQLBuilder.EQUALS, annoSelezionato);
-            clauses.addClause("AND","esercizioOriginale", SQLBuilder.EQUALS, annoSelezionato);
             setBaseclause(clauses);
 
             super.init(config,context);
