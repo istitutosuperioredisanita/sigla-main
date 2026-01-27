5a886cd3339d2feb1d11391a3e3567b3450b2050
tomada_valentina <tomada_valentina@iss.it>
2026-01-27 15:44:46 +0100

    CONFIGURAZIONE CNR - Fix ricerca obbligatoria per esercizio

--- sigla-ejb/src/main/java/it/cnr/contab/config00/action/GestConfCNRAction.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/action/GestConfCNRAction.java
@@ -2,30 +2,26 @@ package it.cnr.contab.config00.action;
 
 import it.cnr.contab.config00.bp.GestConfCNRBP;
 import it.cnr.contab.config00.bulk.Configurazione_cnrBulk;
-import it.cnr.contab.inventario01.bp.CRUDCaricoInventarioBP;
-import it.cnr.contab.inventario01.bulk.Buono_carico_scaricoBulk;
 import it.cnr.jada.action.ActionContext;
-import it.cnr.jada.action.BusinessProcessException;
 import it.cnr.jada.action.Forward;
-import it.cnr.jada.bulk.OggettoBulk;
 import it.cnr.jada.util.action.CRUDAction;
-import it.cnr.jada.util.action.CRUDBP;
-
-import java.rmi.RemoteException;
 
 public class GestConfCNRAction extends CRUDAction {
 
-    public Forward doChangeEsercizio(ActionContext actioncontext) throws RemoteException {
-        try{
-            GestConfCNRBP bp = (GestConfCNRBP)actioncontext.getBusinessProcess();
-            Configurazione_cnrBulk model=(Configurazione_cnrBulk)bp.getModel();
+    public Forward doCerca(ActionContext context) throws java.rmi.RemoteException, InstantiationException, javax.ejb.RemoveException {
 
-            fillModel(actioncontext);
-            bp.setModel(actioncontext,model);
-            return actioncontext.findDefaultForward();
-        }catch (Throwable t) {
-            return handleException(actioncontext, t);
+        try {
+            GestConfCNRBP bp = (GestConfCNRBP) context.getBusinessProcess();
+            Configurazione_cnrBulk conf =(Configurazione_cnrBulk)bp.getModel();
+            fillModel(context);
 
+            if(conf.getEsercizio() == null){
+                throw new it.cnr.jada.comp.ApplicationException("Attenzione! impostare l'esercizio");
+            }
+            return super.doCerca(context);
+        } catch (Exception e) {
+            return handleException(context,e);
         }
     }
+
 }
