02252748fd844b078ed4078e14c5240dadbd95d5
salvio_ciro <cirosalvio@hotmail.com>
2026-03-04 19:06:51 +0100

    Modificata la visualizzazione adesso estrae anche buoni di carico o scarico e beni non associati a fatture

--- sigla-ejb/src/main/java/it/cnr/contab/inventario00/docs/bulk/V_ass_inv_bene_fatturaHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/inventario00/docs/bulk/V_ass_inv_bene_fatturaHome.java
@@ -20,13 +20,55 @@
 * Date 30/08/2005
 */
 package it.cnr.contab.inventario00.docs.bulk;
+
+import it.cnr.contab.util.Utility;
+import it.cnr.jada.UserContext;
 import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
 import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.FindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.rmi.RemoteException;
+
 public class V_ass_inv_bene_fatturaHome extends BulkHome {
 	public V_ass_inv_bene_fatturaHome(java.sql.Connection conn) {
 		super(V_ass_inv_bene_fatturaBulk.class, conn);
 	}
 	public V_ass_inv_bene_fatturaHome(java.sql.Connection conn, PersistentCache persistentCache) {
 		super(V_ass_inv_bene_fatturaBulk.class, conn, persistentCache);
+	}
+
+	@Override
+	public SQLBuilder selectByClause(UserContext usercontext, CompoundFindClause compoundfindclause) throws PersistencyException {
+		SQLBuilder sql =super.selectByClause(usercontext, compoundfindclause);
+		String cds = it.cnr.contab.utenze00.bp.CNRUserContext.getCd_cds(usercontext);
+		String uo = it.cnr.contab.utenze00.bp.CNRUserContext.getCd_unita_organizzativa(usercontext);
+		Integer esercizio = it.cnr.contab.utenze00.bp.CNRUserContext.getEsercizio(usercontext);
+		it.cnr.contab.inventario00.ejb.IdInventarioComponentSession h = (it.cnr.contab.inventario00.ejb.IdInventarioComponentSession)
+				it.cnr.jada.util.ejb.EJBCommonServices.createEJB(
+						"CNRINVENTARIO00_EJB_IdInventarioComponentSession",
+						it.cnr.contab.inventario00.ejb.IdInventarioComponentSession.class);
+
+		try {
+			sql.addSQLClause("AND","esercizio",sql.EQUALS,esercizio);
+			if(!Utility.createCdrComponentSession().isEnte(usercontext)){
+				h.findInventarioFor(usercontext,cds,uo,false);
+				sql.addSQLClause("AND", "pg_inventario", sql.EQUALS, h.findInventarioFor(usercontext,cds,uo,false).getPg_inventario());
+				sql.openParenthesis( FindClause.AND.concat(" ("));
+					sql.addSQLClause("AND", "esercizio_fatt_pass", sql.EQUALS, esercizio);
+					sql.addSQLClause("AND", "cd_cds_fatt_pass", sql.EQUALS, cds);
+				sql.closeParenthesis();
+				sql.addSQLClause("OR", "esercizio_fatt_pass", sql.ISNULL,null);
+				sql.closeParenthesis();
+			}
+			return sql;
+		} catch (ComponentException e) {
+			throw new RuntimeException(e);
+		} catch (RemoteException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
