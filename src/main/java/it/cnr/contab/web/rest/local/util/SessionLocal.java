28e63d7031bf4a2b718bd479f9415f268e33fb4f
marco.spasiano <marco.spasiano@cnr.it>
2026-06-16 09:38:36 +0200

    new feature: add session destroy endpoint
    Add POST /session/destroy endpoint to invalidate the current
    HttpSession, to be called by the frontend on tab close
    (beforeunload) for proactive server-side session cleanup.

---
+++ src/main/java/it/cnr/contab/web/rest/local/util/SessionLocal.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.web.rest.local.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/session")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Tag(name = "Session", description = "Metodi utili alla gesuione della Sessione HTTP")
+public interface SessionLocal {
+
+    @POST
+    @Path("/destroy")
+    Response destroy(@Context HttpServletRequest request) throws Exception;
+
+}
