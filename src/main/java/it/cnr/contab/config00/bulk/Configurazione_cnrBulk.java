0e273f4435af88615c4f70512d7cf7ee0c4f2a7f
csalvio <ciro.salvio@iss.it>
2026-08-04 13:26:10 +0200

    Sostituito il costruttore deprecato new Integer(0)) con Integer.valueOf(0)

--- src/main/java/it/cnr/contab/config00/bulk/Configurazione_cnrBulk.java
+++ src/main/java/it/cnr/contab/config00/bulk/Configurazione_cnrBulk.java
@@ -395,7 +395,7 @@ public class Configurazione_cnrBulk extends Configurazione_cnrBase {
     }
     public void caricaEsercizioList(UserContext usercontext) {
 
-        this.getEsercizioList().put(new Integer(0), new Integer(0));
+        this.getEsercizioList().put(Integer.valueOf(0), Integer.valueOf(0));
         Integer e = CNRUserContext.getEsercizio(usercontext).intValue();
         this.getEsercizioList().put(e, e);
     }
