a839439d7633db67ec9ad73903af3bbd76f7ec71
marco.spasiano <marco.spasiano@cnr.it>
2026-09-04 16:43:59 +0200

    fix: test file alredy exists [ci skip]

--- src/test/java/it/cnr/test/h2/ordmag/ordini/bp/CRUDOrdineAcqBP003.java
+++ src/test/java/it/cnr/test/h2/ordmag/ordini/bp/CRUDOrdineAcqBP003.java
@@ -30,7 +30,11 @@ import org.openqa.selenium.support.ui.Select;
 import org.wildfly.common.Assert;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Test di:
@@ -240,7 +244,7 @@ public class CRUDOrdineAcqBP003 extends ActionDeployments {
      */
     @Test
     @Order(3)
-    public void testEvasioneConsegna001() {
+    public void testEvasioneConsegna001() throws IOException {
         switchToFrameMenu();
         doSelezionaMenu(ORD_EVAORD);
 
@@ -291,7 +295,9 @@ public class CRUDOrdineAcqBP003 extends ActionDeployments {
         String textAlert = handleTextAlert(browser);
         Assertions.assertEquals("Attenzione: è obbligatorio allegare il Documento di Trasporto (DDT).", textAlert);
 
-        File file = new File("src/test/resources/contratto.pdf");
+        File original = new File("src/test/resources/contratto.pdf");
+        File file = new File(original.getParentFile(), "contratto".concat(UUID.randomUUID().toString()).concat(".pdf"));
+        Files.copy(original.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
         doClickButton("doTab('tab','tabAllegati')");
         doClickButton("doAddToCRUD(main.ArchivioAllegati)");
         Select select = new Select(getGrapheneElement("main.ArchivioAllegati.aspectName"));
@@ -536,7 +542,7 @@ public class CRUDOrdineAcqBP003 extends ActionDeployments {
      */
     @Test
     @Order(8)
-    public void testEvasioneConsegna002() {
+    public void testEvasioneConsegna002() throws IOException {
         switchToFrameMenu();
         doSelezionaMenu(ORD_EVAORD);
 
@@ -587,7 +593,9 @@ public class CRUDOrdineAcqBP003 extends ActionDeployments {
         String textAlert = handleTextAlert(browser);
         Assertions.assertEquals("Attenzione: è obbligatorio allegare il Documento di Trasporto (DDT).", textAlert);
 
-        File file = new File("src/test/resources/contratto.pdf");
+        File original = new File("src/test/resources/contratto.pdf");
+        File file = new File(original.getParentFile(), "contratto".concat(UUID.randomUUID().toString()).concat(".pdf"));
+        Files.copy(original.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
         doClickButton("doTab('tab','tabAllegati')");
         doClickButton("doAddToCRUD(main.ArchivioAllegati)");
         Select select = new Select(getGrapheneElement("main.ArchivioAllegati.aspectName"));
