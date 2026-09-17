5ea4e6ca0c2bcc24fa807ea5a32669e14a990597
csalvio <ciro.salvio@iss.it>
2026-09-17 11:53:20 +0200

    Fix:Visualizzazione e gestione degli allegati delle obbligazioni residue

--- src/main/java/it/cnr/contab/doccont00/core/bulk/ObbligazioneResBulk.java
+++ src/main/java/it/cnr/contab/doccont00/core/bulk/ObbligazioneResBulk.java
@@ -214,14 +214,6 @@ public class ObbligazioneResBulk extends ObbligazioneBulk {
 		);
 	}
 
-	@Override
-	public String getStorePath() {
-		return Arrays.asList(getBasePath(),
-				this.getCd_uo_origine() + "-" + this.getEsercizio_originale() + this.getPg_obbligazione()
-		).stream().collect(
-				Collectors.joining(StorageDriver.SUFFIX)
-		);
-	}
 
 	@Override
 	public String getCMISFolderName() {
